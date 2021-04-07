930601e3568fe23f0e01ca125146e7e2cdb61ce3
charlottedrb <c.derbaghdassarian@gmail.com>
2021-04-07 21:36:32 +0200

    quizz - TODO : fetch randos after navigation

--- lib/view/survey/SurveyView.dart
+++ lib/view/survey/SurveyView.dart
@@ -6,121 +6,193 @@ import 'package:mvvm_flutter_app/view/survey/SurveyViewModel.dart';
 import 'package:mvvm_flutter_app/widget/podometer/Podometre.dart';
 import 'package:stacked/stacked.dart';
 
-int quizz;
+int _quizz = null;
+Map<String, String> answers = {};
+int status = 0;
+var futureRandos;
 
-class SurveyView extends StatelessWidget {
-  const SurveyView({Key key}) : super(key: key);
+getRandos() async {
+  futureRandos = await Rando;
+}
 
-  void _handleRadioValueChange(int val){
-    quizz = val;
-  }
 
+class SurveyView extends StatefulWidget {
+  @override
+  _SurveyViewState createState() => _SurveyViewState();
+}
+
+class _SurveyViewState extends State<SurveyView> {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SurveyViewModel>.reactive(
-      builder: (context, model, child) => Scaffold(
-          body: Container(
-            decoration: BoxDecoration(
-                gradient: RadialGradient(
-                    colors: [Color(0xFF2AB7F6), Color(0xFF5EC8F8), Color(0xFFCAE67B)],
-                    stops: [0, 0.2, 1],
-                    center: Alignment(1, 2),
-                    focal: Alignment.bottomRight,
-                    focalRadius: 2
-                )
-            ),
-            child: Center(
-                child: Column(
-                    children: [
-                      Container(
-                          margin: EdgeInsets.only(left: 40, right: 40, top: 60, bottom: 40),
-                          child: Column(
-                              children: [
-                                Text(
-                                  model.header,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 25,
-                                    color: Colors.white
-                                  )
-                                ),
-                                SizedBox(height: 50),
-                                Text(
-                                  model.heading,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 19,
-                                    fontWeight: FontWeight.w300
-                                  )
-                                ),
-                                SizedBox(height: 50),
-                                Text(
-                                  model.surveyStart,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 35
-                                  )
-                                ),
-                                SizedBox(height: 25),
-                                RadioListTile(
-                                  value: 1,
-                                  groupValue: quizz,
-                                  onChanged: (val) => _handleRadioValueChange(val),
-                                  title: Text(
-                                    model.survey1,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20
-                                    )
-                                  )
-                                ),
-                                RadioListTile(
-                                    value: 2,
-                                    groupValue: quizz,
-                                    onChanged: null,
-                                    title: Text(
-                                        model.survey2,
+      builder: (context, model, child) =>
+          Scaffold(
+              body: Container(
+                decoration: BoxDecoration(
+                    gradient: RadialGradient(
+                        colors: [
+                          Color(0xFF2AB7F6),
+                          Color(0xFF5EC8F8),
+                          Color(0xFFCAE67B)
+                        ],
+                        stops: [0, 0.2, 1],
+                        center: Alignment(1, 2),
+                        focal: Alignment.bottomRight,
+                        focalRadius: 2
+                    )
+                ),
+                child: Center(
+                    child: Column(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(
+                                  left: 40, right: 40, top: 60, bottom: 40),
+                              child: Column(
+                                  children: [
+                                    Text(
+                                        model.header,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 25,
+                                            color: Colors.white
+                                        )
+                                    ),
+                                    SizedBox(height: 50),
+                                    Text(
+                                        model.heading,
                                         style: TextStyle(
                                             color: Colors.white,
-                                            fontSize: 20
+                                            fontSize: 19,
+                                            fontWeight: FontWeight.w300
                                         )
-                                    )
-                                ),
-                                RadioListTile(
-                                    value: 3,
-                                    groupValue: quizz,
-                                    onChanged: null,
-                                    title: Text(
-                                        model.survey3,
+                                    ),
+                                    SizedBox(height: 50),
+                                    Text(
+                                        model.question,
                                         style: TextStyle(
                                             color: Colors.white,
-                                            fontSize: 20
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 35
                                         )
-                                    )
-                                ),
-                                SizedBox(height: 50),
-                                OutlineButton(
-                                  onPressed: () async {
-                                    final futureRandos = await Rando.fetchRandos();
-                                    Navigator.pushNamed(context, core, arguments: {"selectedIndex": 1,"randosCollection" : futureRandos });
-                                  },
-                                  child: Text('Passer cette étape', style: TextStyle(color: Colors.white)),
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-                                  borderSide: BorderSide(
-                                    color: Colors.white,
-                                  ),
-                                )
-                              ]
-                          )
-                      ),
-                    ]
-                )
-            ),
-          )
-      ),
+                                    ),
+                                    SizedBox(height: 25),
+                                    RadioListTile(
+                                        value: 1,
+                                        groupValue: _quizz,
+                                        onChanged: (newValue) {
+                                          setState(() {
+                                            _quizz = newValue;
+                                            answers.addAll({model.question: model.answer1});
+                                            print(answers);
+                                          });
+                                        },
+                                        title: Text(
+                                            model.answer1,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20
+                                            )
+                                        )
+                                    ),
+                                    RadioListTile(
+                                        value: 2,
+                                        groupValue: _quizz,
+                                        onChanged: (newValue) {
+                                          setState(() {
+                                            _quizz = newValue;
+                                            answers.addAll({model.question: model.answer2});
+                                            print(answers);
+                                          });
+                                        },
+                                        title: Text(
+                                            model.answer2,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20
+                                            )
+                                        )
+                                    ),
+                                    RadioListTile(
+                                        value: 3,
+                                        groupValue: _quizz,
+                                        onChanged: (newValue) {
+                                          setState(() {
+                                            _quizz = newValue;
+                                            answers.addAll({model.question: model.answer3});
+                                            print(answers);
+                                          });
+                                        },
+                                        title: Text(
+                                            model.answer3,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20
+                                            )
+                                        )
+                                    ),
+                                    SizedBox(height: 50),
+
+                                    if(_quizz != null) OutlineButton(
+                                      onPressed: () {
+                                        model.changeQuestions();
+                                        // if status 4 appelle storeAnswers() avec un navigation a la fin
+                                        setState(() {
+                                          _quizz = null;
+                                          status++;
+                                          if(status == 3) {
+                                            model.storeAnswers(answers);
+                                            Navigator.pushNamed(context, mapRando,
+                                                arguments: {
+                                                  "selectedIndex": 1,
+                                                  "randosCollection": futureRandos
+                                                });
+                                          }
+                                        });
+                                      },
+                                      child: Text('Continuer',
+                                          style: TextStyle(
+                                              color: Colors.white)),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                              8.0)),
+                                      borderSide: BorderSide(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+
+                                      /*OutlineButton(
+                                      onPressed: () async {
+                                        final futureRandos = await Rando
+                                            .fetchRandos();
+                                        Navigator.pushNamed(context, core,
+                                            arguments: {
+                                              "selectedIndex": 1,
+                                              "randosCollection": futureRandos
+                                            });
+                                      },
+                                      child: Text('Passer cette étape',
+                                          style: TextStyle(
+                                              color: Colors.white)),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                              8.0)),
+                                      borderSide: BorderSide(
+                                        color: Colors.white,
+                                      ),
+                                    )*/
+                                  ]
+                              )
+                          ),
+                        ]
+                    )
+                ),
+              )
+          ),
       viewModelBuilder: () => SurveyViewModel(),
-      onModelReady: (model) => model.debug(),
+      onModelReady: (model) => {
+        model.debug(),
+        model.init()
+      }
     );
   }
 }
--- lib/view/survey/SurveyViewModel.dart
+++ lib/view/survey/SurveyViewModel.dart
@@ -1,15 +1,74 @@
+import 'package:flutter/cupertino.dart';
 import 'package:mvvm_flutter_app/classes/user.dart';
 import 'package:stacked/stacked.dart';
 
 class SurveyViewModel extends BaseViewModel {
+
   String header = 'Bienvenue sur Path Partout, votre nouveau guide personnalisé !';
-  String heading = "pour l'instant cette fonctionalité n'est pas active, merci de cliquer sur passer cette étape";
-  // String heading = 'Afin de vous proposer les randonneées qui vous correspondent le mieux, nous vous proposons un petit quizz.';
-  String surveyStart = 'Je suis...';
-  String survey1 = 'un(e) grand(e) randonneur(euse)';
-  String survey2 = 'randonneur(euse) occasionnel(le)';
-  String survey3 = 'randonneur(euse) débutant';
+  String heading = 'Afin de vous proposer les randonnées qui vous correspondent le mieux, nous vous proposons un petit quizz.';
+
+  int status = 0;
+
+  String question1 = 'Je suis...';
+
+  String answer1_1 = 'un(e) grand(e) randonneur(euse)';
+  String answer2_1 = 'randonneur(euse) occasionnel(le)';
+  String answer3_1 = 'randonneur(euse) débutant';
+
+  String question2 = "D'une durée...";
+  String answer1_2 = 'entre 1h et 3h';
+  String answer2_2 = 'entre 3h et 5h';
+  String answer3_2 = 'plus de 5h';
+
+  String question3 = 'Je préfère...';
+  String answer1_3 = 'bord du lac';
+  String answer2_3 = 'dénivelé';
+  String answer3_3 = 'jolie vue';
+
+  String question;
+  String answer1;
+  String answer2;
+  String answer3;
+
   String btnSkip = 'Passer cette étape';
+
+  init() {
+    this.question = this.question1;
+    this.answer1 = this.answer1_1;
+    this.answer2 = this.answer2_1;
+    this.answer3 = this.answer3_1;
+  }
+
+  changeQuestions() {
+    this.status++;
+    print(this.status);
+
+    switch (this.status) {
+      case 1:
+        {
+          this.question = this.question2;
+          this.answer1 = this.answer1_2;
+          this.answer2 = this.answer2_2;
+          this.answer3 = this.answer3_2;
+        }
+        break;
+      case 2:
+        {
+          this.question = this.question2;
+          this.answer1 = this.answer1_3;
+          this.answer2 = this.answer2_3;
+          this.answer3 = this.answer3_3;
+        }
+        break;
+    }
+  }
+
+  storeAnswers(answers) {
+    print('store Answers');
+    print(answers);
+
+
+  }
 
   debug() {
     User.debuguserfetch();
