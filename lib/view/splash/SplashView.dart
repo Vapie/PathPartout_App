7a4892ec9674ef89b8e6a302a23612a815b369de
kevin <ceriati.kevin1@gmail.com>
2021-04-14 19:38:11 +0200

    merge

--- lib/view/splash/SplashView.dart
+++ lib/view/splash/SplashView.dart
@@ -11,152 +11,8 @@ class SplashView extends StatelessWidget {
   const SplashView({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-              // decoration: BoxDecoration(
-              //     gradient: LinearGradient(
-              //         begin: Alignment.topCenter,
-              //         end: Alignment.bottomCenter,
-              //         colors: [Colors.lightBlue, Colors.lightGreenAccent[100]]
-              //     )
-              // ),
-
-              child: Container(
-                width: double.infinity,
-                height: 350.0,
-                child: Center(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Badge(
-                        toAnimate: false,
-                        shape: BadgeShape.square,
-                        badgeColor: Colors.deepPurple,
-                        borderRadius: BorderRadius.circular(8),
-                        badgeContent: Text('Curieux Aguérrie', style: TextStyle(color: Colors.white)),
-                      ),
-                      SizedBox(width: 80),
-
-                      // Container(
-                      //   foregroundDecoration: BoxDecoration(
-                      //       border: Border(
-                      //         bottom: BorderSide(width: 10, color: Colors.grey))),
-                      // ),
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                          "https://pbs.twimg.com/profile_images/1276124867767738374/JaYqnkUW_400x400.jpg",
-                        ),
-                        radius: 50.0,
-                      ),
-
-                ],
-              ),
-            ),
-          ),
-      ),
-
-
-                Container(
-                  // margin: EdgeInsets.only(top: 100),
-                  child: Column(
-                    children: <Widget>[
-                      Card(
-                        margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.0),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.white,
-                        elevation: 5.0,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 22.0),
-                          child: Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: Column(
-
-                                  children: <Widget>[
-                                    Text(
-                                      "Posts",
-                                      style: TextStyle(
-                                        color: Colors.lightBlueAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "5200",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.lightBlueAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: <Widget>[
-                                    Text(
-                                      "Followers",
-                                      style: TextStyle(
-                                        color: Colors.lightBlueAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "28.5K",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.lightBlueAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-
-                                  children: <Widget>[
-                                    Text(
-                                      "Follow",
-                                      style: TextStyle(
-                                        color: Colors.lightBlueAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "1300",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.lightBlueAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-=======
     return ViewModelBuilder<SplashViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
-
-
           body: GestureDetector(
             onTap: (){Navigator.pushNamed(context, survey);},
             child:  Center(
@@ -169,7 +25,6 @@ class SplashView extends StatelessWidget {
                   widthFactor: 0.3,
                   child: Image.asset(
                     'assets/picture/logo.png',
->>>>>>> 5ba4c35b5ccbd7667730befeff49a828ec74f9b1
                   ),
                 ),
 
@@ -183,62 +38,6 @@ class SplashView extends StatelessWidget {
                   Text(
                     "Bio:",
                     style: TextStyle(
-<<<<<<< HEAD
-                        color: Colors.lightBlueAccent,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 28.0
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text('Arouf gangsta aka roi arouf le plus beau de tous les robeux a grandi à la cité avec ses amis avec lesquels il vendait de la drogue,organisait des braquages, brûlait des voitures et encore de nombreux actes de délinquance.',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Container(
-            width: 300.00,
-
-            child: RaisedButton(
-                onPressed: (){},
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80.0)
-                ),
-                elevation: 0.0,
-                padding: EdgeInsets.all(0.0),
-                child: Ink(
-                      decoration: BoxDecoration(
-                      color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-                      child: GradientButton(
-                      child: Text(
-                      "Modifier",
-                      style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25),
-                      ),
-                      gradient: LinearGradient(colors: [
-                      Colors.lightBlue,
-                      Colors.lightGreenAccent[100]
-                      ]),
-                      )),
-                )
-            ),
-        ],
-      ),
-=======
                         fontWeight: FontWeight.bold,
                         color: Colors.grey,
                         fontSize: 25
@@ -249,12 +48,11 @@ class SplashView extends StatelessWidget {
             )
           ),
           ),
-      ),
+      ])))),
       viewModelBuilder: () => SplashViewModel(),
       onModelReady: (model) => Future.delayed(const Duration(milliseconds: 3000), () {
           Navigator.pushNamed(context, login);
       }),
->>>>>>> 5ba4c35b5ccbd7667730befeff49a828ec74f9b1
     );
   }
 }
